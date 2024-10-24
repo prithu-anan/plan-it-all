18faa5d3f5876cb030f4b05635eb26d379cb69f7
Prithu Anan <prithua00@gmail.com>
2024-10-25 03:35:50 +0600

    done with pdf

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -51,11 +51,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in TypeScript and
-        JavaScript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        Our travel route suggestion website is designed to simplify your journey planning process. With just a few clicks, users can input their desired destination and preferences, and our intelligent algorithm generates optimal travel routes tailored to individual needs. Whether you're looking for the fastest route, scenic paths, or stops along the way for dining and sightseeing, our platform provides detailed itineraries and maps to enhance your travel experience.
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
--- frontend/src/components/PDFViewer.jsx
+++ frontend/src/components/PDFViewer.jsx
@@ -5,24 +5,15 @@ const PDFViewer = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-col items-center gap-5`}
       >
 				{/* Header and Plus Icon */}
-				<div className="flex flex-row justify-between items-center w-full">
-					{/* Header Text */}
-					<div className="flex flex-col items-start gap-5">
-						<h1 className={`${styles.sectionHeadText} text-white`}>PDF Viewer</h1>
-						<p className={`${styles.sectionSubText} text-white`}>
-							View your PDF here
-						</p>
-					</div>
-				</div>
 				{/* PDF Viewer */}
-				<div className="w-full h-full bg-white rounded-xl shadow-card flex justify-center items-center">
+				<div className="rounded-xl shadow-card flex justify-center items-center">
 					<iframe
 						src="../public/Cracking the Coding Interview.pdf"
 						title="pdf"
-						className="w-full h-[120vh] pb-10"
+						className="flex items-center w-[50vw] h-[80vh]"
 					/>
 					</div>
     	</div>
